7214b5239ab3798320e4d05fdb99572fc092456e
grobleen <111244776+grobleen@users.noreply.github.com>
2023-04-16 13:31:08 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -8,12 +8,22 @@ camara = document.getElementById("camara");
 Webcam.attach("#camara");
 function tomarfoto(){
 Webcam.snap(function (data_uri){
-document.getElementById("resultado").innerHTML='<img id="foto" src="'=data_uri+'"/>';
-
+document.getElementById("resultado").innerHTML='<img id="foto" src="'+data_uri+'"/>';
 });    
 }
 console.log("ml5 versión",ml5.version);
 var classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/0yAHPl1X6/model.json',modelloaded);
 function modelloaded(){
 console.log('Modelo cargado');
+}
+function revisar(){
+var img=document.getElementById("foto");
+classifier.classify(img,gotRESULT);    
+}
+function gotRESULT(error,results){
+if (error){
+console.log(error);
+}else{
+console.log(results)    
+}
 }
